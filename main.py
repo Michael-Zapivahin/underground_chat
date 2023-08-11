58269229ef15165ece8f3c4cf2f34bc2950f8d73
Michael-Zapivahin <zapivahin@ya.ru>
2023-08-11 13:42:55 +0300

    write to file

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import asyncio
+import aiofiles
+import sys
+import argparse
+
+from datetime import datetime
 
 
-async def tcp_echo_client():
-    reader, writer = await asyncio.open_connection('minechat.dvmn.org', 5000)
+async def write_chat(args):
+    reader, writer = await asyncio.open_connection(args.host, args.port)
     while True:
-        chunk = await reader.readline()
-        print(chunk.decode('utf-8'))
+        record = await reader.readline()
+        message = f"[{datetime.now().strftime('%d.%m.%y %I:%M')}] {record.decode('utf-8')}"
+        async with aiofiles.open(args.path, 'a', encoding='utf-8') as file:
+            await file.write(message)
+        sys.stdout.write(message)
 
 
 def main():
-    asyncio.run(tcp_echo_client())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-host', '--host', default='minechat.dvmn.org', help='enable logging')
+    parser.add_argument('-port', '--port', default='5000', help='enable delay')
+    parser.add_argument('-history', '--history', default='mine_chat.history', help="History chat's file.")
+    args = parser.parse_args()
+    try:
+        asyncio.run(write_chat(args))
+    except Exception as err:
+        sys.stderr.write(err)
 
 
 if __name__ == '__main__':
