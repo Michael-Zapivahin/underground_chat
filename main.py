0b4ba1ae2078cf056ebc1131b393e6964174d1a6
Michael-Zapivahin <zapivahin@ya.ru>
2023-08-13 14:07:58 +0300

    Add  send message

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import asyncio
+import os
+
 import aiofiles
 import sys
 import argparse
+import time
 
 from datetime import datetime
+from dotenv import load_dotenv
+
+
+async def send_message(args, token):
+    reader, writer = await asyncio.open_connection(args.host, args.port)
+    try:
+        writer.write(f'{token}\r\n'.encode('utf-8'))
+        await writer.drain()
+        writer.write('test message for chat\r\n\n'.encode('utf-8'))
+        await writer.drain()
+    finally:
+        writer.close()
+        await writer.wait_closed()
 
 
 async def write_chat(args):
@@ -17,15 +33,22 @@ async def write_chat(args):
 
 
 def main():
+    load_dotenv()
+    chat_token = os.getenv('CHAT_TOKEN')
     parser = argparse.ArgumentParser()
     parser.add_argument('-host', '--host', default='minechat.dvmn.org', help='enable logging')
     parser.add_argument('-port', '--port', default='5000', help='enable delay')
     parser.add_argument('-history', '--history', default='mine_chat.history', help="History chat's file.")
     args = parser.parse_args()
+    # try:
+    #     asyncio.run(write_chat(args))
+    # except Exception as err:
+    #     sys.stderr.write(err)
     try:
-        asyncio.run(write_chat(args))
+        asyncio.run(send_message(args, chat_token))
     except Exception as err:
         sys.stderr.write(err)
+
 
 
 if __name__ == '__main__':
